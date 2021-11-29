b99de9aa36c003e80250eec87bd46ad2b5e0b064
Mauricio Zanon <mauricio.zanon@getronics.com>
2021-11-28 21:27:18 -0300

    Se empezo a renderizar los tiles con PIXIjs

--- src/vistas/gamescreen/GameScreenVC.tsx
+++ src/vistas/gamescreen/GameScreenVC.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PlayerViewVC from './components/PlayerViewVC';
 import SideBarVC from './components/SideBarVC';
 import PlayerViewController from './controllers/PlayerViewController';
 import './estilosGameScreen.css';
 import { useStore } from '../../store/store';
+import * as PIXI from 'pixi.js';
+import { Application, Graphics } from 'pixi.js';
+import $ from "jquery";
 
 export default function GameScreenVC(): JSX.Element {
 
-  window.addEventListener('keypress', PlayerViewController.resolverKeyDown);
-  const centroPantalla = useStore(state => state.centroPantalla);
+	window.addEventListener('keypress', PlayerViewController.resolverKeyDown);
+	const centroPantalla = useStore(state => state.centroPantalla);
 
-  return (
-    <>
-      <PlayerViewVC centroPantalla={ centroPantalla } />
-      <SideBarVC />
-    </>
-  );
+	console.count("GAMESCREEN");
+
+	return (
+		<>
+			<PlayerViewVC centroPantalla={ centroPantalla } />
+			<SideBarVC />
+		</>
+	);
 
 }
--- src/vistas/gamescreen/components/PlayerViewVC.tsx
+++ src/vistas/gamescreen/components/PlayerViewVC.tsx
@@ -1,79 +1,128 @@
-import React from 'react';
+import { Graphics } from '@pixi/graphics';
+import { Application } from 'pixi.js';
+import React, { useEffect, useState } from 'react';
 import Mapa from '../../../mapa/Mapa';
 import { Posicion, modificarTx, modificarTy } from '../../../mapa/Posicion';
 import Tile from '../../../mapa/Tile';
-import TileVC from './TileVC';
+import * as PIXI from 'pixi.js';
+import $ from "jquery";
 
-type PlayerViewVCProps = { centroPantalla: Posicion; };
+type PlayerViewProps = {
+	centroPantalla: Posicion;
+};
 
-type PlayerViewVCState = { cantidadTiles: number; };
+let app: Application;
 
-export default class PlayerViewVC extends React.Component<PlayerViewVCProps, PlayerViewVCState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cantidadTiles: 25
-    };
-  }
+let cantidadTilesX: number;
+let cantidadTilesY: number;
+let fondos: Graphics[][];
+let simbolos: PIXI.Text[][];
 
-  shouldComponentUpdate(nextProps: PlayerViewVCProps, nextState: PlayerViewVCState): boolean {
-    return this.props.centroPantalla !== nextProps.centroPantalla ||
-      this.state.cantidadTiles !== nextState.cantidadTiles;
-  }
+export default function PlayerViewVC(props: PlayerViewProps): JSX.Element {
 
-  public render(): JSX.Element {
-    return (
-      <div className="player-view" onWheelCapture={ () => this.setState({ cantidadTiles: 35 }) }>
-        { this.generarTiles() }
-      </div >
-    );
-  }
+	const [tamañoTiles, setTamañoTiles] = useState(25);
 
-  // private cambiarZoom(evento: any): void {
-  // 	if (evento.deltaY > 0) {
-  // 		switch (this.state.cantidadTiles) {
-  // 			case 13:
-  // 				this.setState({ cantidadTiles: 25 });
-  // 				break;
-  // 			case 25:
-  // 				this.setState({ cantidadTiles: 35 });
-  // 				break;
-  // 		}
-  // 	} else {
-  // 		switch (this.state.cantidadTiles) {
-  // 			case 35:
-  // 				this.setState({ cantidadTiles: 25 });
-  // 				break;
-  // 			case 25:
-  // 				this.setState({ cantidadTiles: 13 });
-  // 				break;
-  // 		}
-  // 	}
-  // }
+	useEffect(() => {
+		if (!app) {
+			crearApp();
+			calcularCantidadDeTiles();
+			inicializarFondos();
+			inicializarSimbolos();
+		}
+		generarTiles();
+	});
 
-  private generarTiles(): JSX.Element[][] {
-    return this.obtenerMapa().map((columna, index) => this.generarColumna(columna, index));
-  }
+	function crearApp() {
+		app = new PIXI.Application({
+			antialias: true,
+			view: $(".player-view")[0] as HTMLCanvasElement
+		});
+		document.body.appendChild(app.view);
+	}
 
-  private generarColumna(columna: Tile[], indexColumna: number): JSX.Element[] {
-    return columna.map((tile, indexFila) => {
-      return <TileVC tamaño={ this.tamañoTile() } tile={ tile } key={ indexColumna + '-' + indexFila }></TileVC>;
-    });
-  }
+	function calcularCantidadDeTiles() {
+		cantidadTilesX = app.view.width / tamañoTiles;
+		cantidadTilesY = app.view.height / tamañoTiles;
+	}
 
-  private obtenerMapa(): Tile[][] {
-    return Mapa.obtenerAreaCuadrada(this.pos00(), this.state.cantidadTiles, this.state.cantidadTiles);
-  }
+	function inicializarFondos(): void {
+		fondos = [];
+		for (let i = 0; i < cantidadTilesX; i++) {
+			fondos[i] = [];
+			for (let j = 0; j < cantidadTilesY; j++) {
+				const fondo = new Graphics();
+				fondo.beginFill(0x000000);
+				fondo.drawRect(tamañoTiles * i, tamañoTiles * j, tamañoTiles, tamañoTiles);
+				fondo.endFill();
+				app.stage.addChild(fondo);
+				fondos[i][j] = fondo;
+			}
+		}
+	}
 
-  private pos00(): Posicion {
-    const resultado: Posicion = { ...this.props.centroPantalla };
-    const delta: number = Math.ceil(-(this.state.cantidadTiles / 2));
-    modificarTx(resultado, delta);
-    modificarTy(resultado, delta);
-    return resultado;
-  }
+	function inicializarSimbolos(): void {
+		simbolos = [];
+		for (let i = 0; i < cantidadTilesX; i++) {
+			simbolos[i] = [];
+			for (let j = 0; j < cantidadTilesY; j++) {
+				const ascii = new PIXI.Text("");
+				ascii.position.set(tamañoTiles * i, tamañoTiles * j);
+				fondos[i][j].addChild(ascii);
+				simbolos[i][j] = ascii;
+			}
+		}
+	}
 
-  private tamañoTile(): number {
-    return 100 / this.state.cantidadTiles;
-  }
+	function generarTiles(): void {
+		const tiles = obtenerMapa();
+
+		for (let i = 0; i < tiles.length; i++) {
+			for (let j = 0; j < tiles[0].length; j++) {
+				const tile = tiles[i][j];
+
+				actualizarFondo(tile, tamañoTiles, i, j);
+				actualizarAscii(tile, tamañoTiles, i, j);
+
+			}
+		}
+	}
+
+	function obtenerMapa(): Tile[][] {
+		return Mapa.obtenerAreaCuadrada(pos00(), cantidadTilesY, cantidadTilesX);
+	}
+
+	function pos00(): Posicion {
+		const resultado: Posicion = { ...props.centroPantalla };
+		const deltaX: number = Math.ceil(-(cantidadTilesX / 2));
+		const deltaY: number = Math.ceil(-(cantidadTilesY / 2));
+		modificarTx(resultado, deltaX);
+		modificarTy(resultado, deltaY);
+		return resultado;
+	}
+
+	function actualizarFondo(tile: Tile, tamaño: number, i: number, j: number): Graphics {
+		const color = Number(`0x${ tile.colorFondo.substr(1, 6) }`);
+		const fondo = fondos[i][j] || new Graphics();
+		fondo.clear();
+		fondo.beginFill(color);
+		fondo.drawRect(tamaño * i, tamaño * j, tamaño, tamaño);
+		fondo.endFill();
+		return fondo;
+	}
+
+	function actualizarAscii(tile: Tile, tamaño: number, i: number, j: number): PIXI.Text {
+		const ascii = simbolos[i][j] || new PIXI.Text("");
+		ascii.text = tile.simbolo;
+		ascii.style = {
+			fill: tile.colorSimbolo,
+			fontFamily: "Courier New",
+			align: "center",
+			fontSize: tamaño
+		};
+		ascii.anchor.x = -0.5;
+		return ascii;
+	}
+
+	return <canvas className="player-view" onWheelCapture={ () => setTamañoTiles(35) }></canvas >;
+
 }
