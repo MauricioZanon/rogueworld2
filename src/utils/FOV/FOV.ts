b41dfbd21df4e0fb0a9d9fa33900e9dbcdc72518
MauricioZanon <mbz0505@gmail.com>
2024-11-30 16:10:35 -0300

    Limpieza de FOV

--- src/utils/FOV/FOV.ts
+++ src/utils/FOV/FOV.ts
@@ -1,11 +1,7 @@
-/** Guarda la distancia desde un tile origen hasta los tiles cercanos para acelerar el shadow casting  */
-const distancias = crearTablaDeDistancias();
-// private static HashMap<Direction, int[]> octantsByDirection = createOctantsByDirections();
-
 import Entidad from '../../entidades/Entidad';
 import { VisionComp } from '../../entidades/componentes/VisionComp';
 import Mapa from '../../mapa/Mapa';
-import { PosicionGlobal, obtenerGlobal, obtenerLocal } from '../../mapa/Posicion';
+import { obtenerGlobal, obtenerLocal } from '../../mapa/Posicion';
 import Tile from '../../mapa/Tile';
 
 type Slope = {
@@ -14,40 +10,23 @@ type Slope = {
 }
 
 /*
-	 * OCTANTES:	RECORRIDO (desde el centro hacia el extremo)
-	 *  \2 1/		  \> </
-	 *  3\ /0		  V\ /V
-	 * 	4/ \7		  ^/ \^
-	 *  /5 6\		  /> <\
-	 */
-// private static HashMap<Direction, int[]> createOctantsByDirections() {
-// 	HashMap<Direction, int[]> map = new HashMap<>();
-// 	map.put(Direction.N, new int[] {7, 0, 1, 2, 3, 4});
-// 	map.put(Direction.NW, new int[] {0, 1, 2, 3, 4, 5});
-// 	map.put(Direction.W, new int[] {1, 2, 3, 4, 5, 6});
-// 	map.put(Direction.SW, new int[] {2, 3, 4, 5, 6, 7});
-// 	map.put(Direction.S, new int[] {3, 4, 5, 6, 7, 0});
-// 	map.put(Direction.SE, new int[] {4, 5, 6, 7, 0, 1});
-// 	map.put(Direction.E, new int[] {5, 6, 7, 0, 1, 2});
-// 	map.put(Direction.NE, new int[] {6, 7, 0, 1, 2, 3});
-		
-// 	return map;
-// }
+	OCTANTES:	RECORRIDO (desde el centro hacia el extremo)
+	 \2 1/		  \> </
+	 3\ /0		  V\ /V
+		4/ \7		  ^/ \^
+	 /5 6\		  /> <\
+*/
 
 //TODO hacer que los NPC solo calculen la vision de los tiles con alguna entidad importante
 export function calcularFOV(entity: Entidad): void {
 	const vc: VisionComp = entity.visionComp;
 	vc.tilesVisibles = [];
 	
-	const origin: Tile = Mapa.obtenerTile(entity.posicion);
-	vc.tilesVisibles.push(origin);
+	const origen: Tile = Mapa.obtenerTile(entity.posicion);
+	vc.tilesVisibles.push(origen);
 		
-	// int[] octants = octantsByDirection.get(vc.faceDir);
-	// for(int i = 0; i < octants.length; i++) {
-	// 	compute(octants[i], origin, vc.sightRange, 1, new Slope(1, 1), new Slope(1, 0), isTranslucent, addToVisionMap);
-	// }
 	for(let i = 0; i < 8; i++) {
-		compute(i, origin, vc, 1, {X: 1, Y: 1}, {X: 1, Y: 0});
+		calcular(i, origen, vc, 1, {X: 1, Y: 1}, {X: 1, Y: 0});
 	}
 }
 
@@ -58,18 +37,18 @@ function esOpaco(tile: Tile): boolean {
 const correccionSlopeInicial = 0.15;
 const correccionNuevoSlope = 0.25;
 	
-function compute(octant: number, origin: Tile, vc: VisionComp, x: number, top: Slope, bottom: Slope) {
+function calcular(octante: number, origen: Tile, vc: VisionComp, x: number, top: Slope, bottom: Slope) {
 	const {rango, tilesVisibles} = vc;
 
 	while(x <= rango) {
 		const topY = top.X == 1 ? x : Math.round(((((x * 2) + correccionSlopeInicial) * top.Y) + top.X - correccionSlopeInicial) / (top.X * 2));
 		const bottomY = bottom.Y == 0 ? 0 : Math.round(((((x * 2) - correccionSlopeInicial) * bottom.Y) + bottom.X) / (bottom.X * 2));
 		
-		let brokeCondition = -1; // 0:false, 1:true, -1:not applicable
+		let interrumpeVision = -1; // 0:false, 1:true, -1:not applicable
 		for (let y = topY; y >= bottomY; y--) {
-			const posGlobal = obtenerGlobal(origin.posicion);
+			const posGlobal = obtenerGlobal(origen.posicion);
 			
-			switch (octant) {
+			switch (octante) {
 				case 0:
 					posGlobal.x += x;
 					posGlobal.y -= y;
@@ -104,53 +83,35 @@ function compute(octant: number, origin: Tile, vc: VisionComp, x: number, top: S
 					break;
 			}
 			
-			const evaluatedTile: Tile = Mapa.obtenerTile(obtenerLocal(posGlobal));
-			if(!tilesVisibles.includes(evaluatedTile)) {
-				tilesVisibles.push(evaluatedTile);
+			const tileEvaluado: Tile = Mapa.obtenerTile(obtenerLocal(posGlobal));
+			if(!tilesVisibles.includes(tileEvaluado)) {
+				tilesVisibles.push(tileEvaluado);
 			}
 
 			if (x <= rango) {
-				if (esOpaco(evaluatedTile)) {
-					if (brokeCondition == 0) {
+				if (esOpaco(tileEvaluado)) {
+					if (interrumpeVision == 0) {
 						const newBottom: Slope = {X: (x) + correccionNuevoSlope, Y: (y) + correccionNuevoSlope};
 						if (y == bottomY) {
 							bottom = newBottom;
 							break;
 						} else {
-							compute(octant, origin, vc, x+1, top, newBottom);
+							calcular(octante, origen, vc, x+1, top, newBottom);
 						}
 					}
-					brokeCondition = 1;
+					interrumpeVision = 1;
 				} else {
-					if (brokeCondition == 1){
+					if (interrumpeVision == 1){
 						top = {X: (x) + correccionNuevoSlope, Y: (y) + correccionNuevoSlope};
 					}
-					brokeCondition = 0;
+					interrumpeVision = 0;
 				}
 			}
 		}
 
-		if (brokeCondition != 0) {
+		if (interrumpeVision != 0) {
 			break; // if the column ended in a clear tile, continue processing the current sector
 		}
 		x++;
 	}
 }
-	
-function crearTablaDeDistancias(): number[][] {
-	const tabla = [];
-		
-	for(let x = 0; x < 40; x++) {
-		tabla[x] = [];
-		for(let y = 0; y < 40; y++) {
-			tabla[x][y] = Math.sqrt((x*x) + (y*y));
-		}
-	}
-		
-	return tabla;
-}
-	
-// function obtenerDistancia(pos1: PosicionGlobal, pos2: PosicionGlobal) {
-// 	return distancias[Math.abs(pos1.x - pos2.x)][Math.abs(pos1.y - pos2.y)];
-// }
-
--- src/utils/path-finding/AStar.ts
+++ src/utils/path-finding/AStar.ts
@@ -106,8 +106,8 @@ class MinHeap {
 		const length = this.heap.length;
 		const element = this.heap[0];
 		while (true) {
-			const leftChildIndex = 2 * index + 1;
-			const rightChildIndex = 2 * index + 2;
+			const leftChildIndex = (2 * index) + 1;
+			const rightChildIndex = (2 * index) + 2;
 			let swap = null;
 
 			if (leftChildIndex < length) {
